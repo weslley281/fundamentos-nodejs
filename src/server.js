e9043d53865b9900ff2ec7bee9d806d68fdaff58
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-01-14 16:42:45 -0400

    a

--- src/server.js
+++ src/server.js
@@ -1,17 +1,26 @@
 import http from 'node:http';
 
+const users = [];
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuarios');
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users));
   }
 
-  if (method === 'Post' && url === '/users') {
-    return res.end('Criação de usuários');
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+    });
+
+    return res.end('Criação de usuário');
   }
 
-  return res.end('Olá mundo23');
+  return res.end('Hello World');
 });
-
 server.listen(5000);
