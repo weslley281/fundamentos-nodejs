12a5b92af35a05ecc4432d9508ea36e6c622da96
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-02-12 05:50:58 -0400

    a

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,13 +1,9 @@
-// process.stdin.pipe(process.stdout);
-
-import { Readable } from 'node:stream';
+import { Readable, Writable, Transform } from 'node:stream';
 
 class OneToHundredStream extends Readable {
   index = 1;
-
   _read() {
     const i = this.index++;
-
     setTimeout(() => {
       if (i > 100) {
         this.push(null);
@@ -15,8 +11,25 @@ class OneToHundredStream extends Readable {
         const buf = Buffer.from(String(i));
         this.push(buf);
       }
-    }, 5000);
+    }, 1000);
   }
 }
 
-new OneToHundredStream().pipe(process.stdout);
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream());
