72ff5cc399a8fb3d0065862282d6c9a8ea281745
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-03-20 15:27:32 -0400

    a

--- src/server.js
+++ src/server.js
@@ -1,32 +1,19 @@
 import http from 'node:http';
-import { Database } from './database.js';
-import { json } from './middlewares/json.js';
 
-const database = new Database();
+import { json } from './middlewares/json.js';
+import { routes } from './routes.js';
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res);
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users');
+  const route = routes.find((route) => {
+    return route.method === method && route.path === url;
+  });
 
-    return res.end(JSON.stringify(users));
-  }
-
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body;
-
-    const user = {
-      id: 1,
-      name,
-      email,
-    };
-
-    database.insert('users', user);
-
-    return res.writeHead(201).end();
+  if (route) {
+    return route.handler(req, res);
   }
 
   return res.writeHead(404).end();
